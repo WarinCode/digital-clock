02cacacde17d8d6dcb2deefdc68ea00345cda0dd
VarinCode <94356640+VarinCode@users.noreply.github.com>
2023-07-16 00:38:26 +0700

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import Clock from "./components/Clock";
 import "./scss/style.scss"
 
-const app:HTMLDivElement = document.querySelector("#app")!;
+const app:HTMLElement = document.querySelector("#app")!;
 const s:number = 1000;
 
 setInterval(():void => { // callback function ทุกๆ 1วินาที
